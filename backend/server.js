493c5ca235a993ab780e4eacfabfb5e2c66e6e37
kamrulcoder <kamrulcoder@gmail.com>
2022-06-12 19:14:56 +0600

    User Data Update Api Function Create

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,12 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 app.use(express.json());
 
 const URI = process.env.MONGO_URL;
-
-
 
 mongoose.connect(URI, {
     useNewUrlParser: true, 
@@ -18,11 +17,10 @@ mongoose.connect(URI, {
     if(err) throw err;
     console.log('Connected to MongoDB!!!')
  })
-
-  
+ 
 
  app.use("/api/auth", authRoute);
-
+ app.use("/api/users", userRoute);
 
 
 app.listen("5000", () => {
