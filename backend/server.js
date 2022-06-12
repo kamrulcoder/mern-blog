784c43e50bfeb65ad2ed0032bb555dc2317a904b
kamrulcoder <kamrulcoder@gmail.com>
2022-06-12 19:48:19 +0600

    Image Upload Functionality  Api

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const categoryRoute = require("./routes/categories");
 const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path");
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
 
 
 dotenv.config();
@@ -20,6 +24,21 @@ mongoose.connect(URI, {
     if(err) throw err;
     console.log('Connected to MongoDB!!!')
  })
+
+ 
+const storage = multer.diskStorage({
+   destination: (req, file, cb) => {
+     cb(null, "images");
+   },
+   filename: (req, file, cb) => {
+     cb(null, req.body.name);
+   },
+ });
+ 
+ const upload = multer({ storage: storage });
+ app.post("/api/upload", upload.single("file"), (req, res) => {
+   res.status(200).json("File has been uploaded");
+ });
  
 
  app.use("/api/auth", authRoute);
